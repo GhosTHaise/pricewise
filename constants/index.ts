80adc82a07238282cc80d93b4b8ed1b0eb6c466b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-03 19:02:07 +0300

    create constants

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const navIcons : {
+    src : string;
+    alt : string;
+}[] = [
+    {
+        src : "assets/icons/search.svg",
+        alt : "search"
+    },
+    {
+        src : "assets/icons/black-heart.svg",
+        alt : "heart"
+    },
+    {
+        src : "assets/icons/user.svg",
+        alt : "user"
+    }
+]
