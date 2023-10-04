94aab498348b0b610fe72c27ebbfddfc69a742e7
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-04 11:25:25 +0300

    searchbar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,8 +1,28 @@
+"use client"
 import React from 'react'
 
 const Searchbar = () => {
+    const handleSubmit = () => {
+
+    }
+
   return (
-    <div>Searchbar</div>
+    <form
+        className='flex flex-wrap gap-4 mt-12'
+        onSubmit={handleSubmit}
+    >
+        <input 
+            type="text" 
+            className='searchbar-input' 
+            placeholder='Enter product link' 
+        />
+        <button
+            type='submit'
+            className='searchbar-btn'
+        >
+            Search
+        </button>
+    </form>
   )
 }
 
