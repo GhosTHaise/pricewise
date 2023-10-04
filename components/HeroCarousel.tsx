02748340bb47424eb46563f968cba731c2a4c470
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-04 12:59:54 +0300

    build caroussel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+"use client"
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { heroImages } from "@/constants";
+import Image from "next/image";
 
 const HeroCarousel = () => {
   return (
-    <div>HeroCarousel</div>
+    <div className="hero-carousel">
+        <Carousel
+            showThumbs={false}
+            autoPlay
+            infiniteLoop 
+            interval={2000}
+            showArrows={false}
+            showStatus={false}
+        >
+        {
+            heroImages.map(image => (
+                <Image 
+                    key={image.alt}
+                    src={image.imgUrl}
+                    alt={image.alt}
+                    width={484}
+                    height={484}
+                    className="object-contain"
+                />
+            ))
+        }
+        </Carousel>
+    </div>
   )
 }
 
--- constants/index.ts
+++ constants/index.ts
@@ -15,3 +15,26 @@ export const navIcons : {
         alt : "user"
     }
 ]
+
+export const heroImages : {imgUrl : string;alt : string;}[] = [
+    {
+        imgUrl : "/assets/images/hero-1.svg",
+        alt : "smartwatch"
+    },
+    {
+        imgUrl : "/assets/images/hero-2.svg",
+        alt : "bag"
+    },
+    {
+        imgUrl : "/assets/images/hero-3.svg",
+        alt : "lamp"
+    },
+    {
+        imgUrl : "/assets/images/hero-4.svg",
+        alt : "air fryer"
+    },
+    {
+        imgUrl : "/assets/images/hero-5.svg",
+        alt : "chair"
+    },
+]
