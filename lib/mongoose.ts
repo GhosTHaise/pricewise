bb94e539e90a65c395ee3851f7febff367faceeb
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-10 12:01:01 +0300

    connect to mongoDb atlas

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,5 +3,19 @@ import mongoose from "mongoose";
 let isConnected = false; //variable to track the connection status
 
 export const connectToDb = async () => {
+    mongoose.set("strictQuery" , true);
+    if(!process.env.NEXT_MONGODB_URI) return console.log("MONGODB URI is not defined");
+    
+    if(isConnected) return console.log("=> using existing database connection .");
+    
+    try {
+        await mongoose.connect(process.env.NEXT_MONGODB_URI);
 
+        isConnected = true;
+
+        console.log("=> MongoDb connected !");
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
