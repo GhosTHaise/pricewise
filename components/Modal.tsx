8eab72c7c178949a0a70d373a75e6fd103751c0c
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-14 21:18:29 +0300

    Update Modal.tsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState , Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import Image from 'next/image'
 
 const Modal = () => {
   
@@ -24,20 +25,60 @@ const Modal = () => {
                 onClose={() => setIsOpen(false)}
                 className="dialog-container"    
             >
-                <Dialog.Panel>
-                    <Dialog.Title>Deactivate account</Dialog.Title>
-                    <Dialog.Description>
-                    This will permanently deactivate your account
-                    </Dialog.Description>
-
-                    <p>
-                    Are you sure you want to deactivate your account? All of your data
-                    will be permanently removed. This action cannot be undone.
-                    </p>
-
-                    <button onClick={() => setIsOpen(false)}>Deactivate</button>
-                    <button onClick={() => setIsOpen(false)}>Cancel</button>
-                </Dialog.Panel>
+                <div
+                    className='min-h-screen px-4 text-center'
+                >
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom='opacity-0'
+                        enterTo="opacity-100"
+                        leave='ease-in duration 200'
+                        leaveFrom='opacity-100'
+                        leaveTo='opacity-0'
+                    >
+                        <Dialog.Overlay
+                            className="fixed inset-0"
+                        />
+                    </Transition.Child>
+                    {/** Tricks on Headless UI */}
+                    <span
+                        className='inline-block h-screen align-middle'
+                        aria-hidden="true"
+                    />
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom='opacity-0 scale-95'
+                        enterTo='opacity-100 sclale-100'
+                        leave='ease-in duration-200'
+                        leaveFrom='opacity-100 scale-100'
+                        leaveTo='opacity-0 scale-95'
+                    >
+                        <div
+                            className='dialog-content'
+                        >
+                            <div
+                                className='flex flex-col'
+                            >
+                                <div
+                                    className='flex justify-between'
+                                >
+                                    <div
+                                        className='p-3 border border-gray-200 rounded-10'
+                                    >
+                                        <Image
+                                            src="/assets/icons/logo.svg"
+                                            alt="logo"
+                                            width={28}
+                                            height={28}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Transition.Child>
+                </div>
             </Dialog>
         </Transition>
     </>
