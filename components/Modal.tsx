9cd24ddecdc6c7d33047ecf6bc1d2596ead8acab
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-10-15 19:43:50 +0300

    add modal items

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -74,7 +74,60 @@ const Modal = () => {
                                             height={28}
                                         />
                                     </div>
+                                    <Image
+                                        src="/assets/icons/x-close.svg"
+                                        alt='close' 
+                                        width={24}
+                                        height={24}
+                                        className='cursor-pointer'
+                                        onClick={() => setIsOpen(false)}    
+                                    />
                                 </div>
+                                <h4
+                                    className='dialog-head_text'
+                                >
+                                    Stay updated with product pricing alerts right in 
+                                    your inbox!
+                                </h4>
+                                <p
+                                    className='text-sm text-gray-600 mt-2'
+                                >
+                                    Never miss a bargain again with our timely alerts!
+                                </p>
+
+                                <form
+                                    className='flex flex-col mt-5'
+                                >
+                                    <label 
+                                        htmlFor="email"
+                                        className='text-sm font-medium text-gray-700'    
+                                    >
+                                        Email address
+                                    </label>
+                                    <div
+                                        className='dialog-input_container'
+                                    >
+                                        <Image
+                                            src="/assets/icons/mail.svg"
+                                            alt='mail'
+                                            width={18}
+                                            height={18}
+                                        />
+                                        <input 
+                                            type="text" 
+                                            required 
+                                            id="email"
+                                            placeholder='contact@ghost.com'    
+                                            className='dialog-input'
+                                        />
+                                    </div>
+                                    <button
+                                        type='submit'
+                                        className='dialog-btn'
+                                    >
+                                        Track
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </Transition.Child>
